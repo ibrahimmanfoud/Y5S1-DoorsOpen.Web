da83f0a6b61466f751ca83f5c7ea0f30d828bd5d
Caitlin Bender <85584674+coolitnow@users.noreply.github.com>
2021-12-07 13:06:04 -0500

    Add Form to upload file through Azure/ to database

--- DoorsOpen/Controllers/BuildingImageController.cs
+++ DoorsOpen/Controllers/BuildingImageController.cs
@@ -7,16 +7,21 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using DoorsOpen.Data;
 using DoorsOpen.Models;
+using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 
 namespace DoorsOpen.Controllers
 {
     public class BuildingImageController : Controller
     {
         private readonly SiteDbContext _context;
+        private readonly IConfiguration _config;
 
-        public BuildingImageController(SiteDbContext context)
+        public BuildingImageController(SiteDbContext context, IConfiguration configuration)
         {
             _context = context;
+            _config = configuration;
         }
 
         // GET: BuildingImage
@@ -56,10 +61,17 @@ namespace DoorsOpen.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,BuildingId,ImageURL,AltText")] BuildingImageModel buildingImageModel)
+        public async Task<IActionResult> Create([Bind("Id,BuildingId,ImageURL,AltText")] BuildingImageModel buildingImageModel, IFormFile upload)
         {
             if (ModelState.IsValid)
             {
+                if (upload != null)
+                {
+                    string imageName = GetFileName(upload);
+                    buildingImageModel.ImageURL = imageName;
+                    UploadToAzure(imageName, upload);
+                }
+
                 _context.Add(buildingImageModel);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
@@ -151,6 +163,57 @@ namespace DoorsOpen.Controllers
         private bool BuildingImageModelExists(int id)
         {
             return _context.BuildingImages.Any(e => e.Id == id);
+        }
+
+
+        public string GetFileName(IFormFile upload)
+        {
+            int indexExt = 0;
+            string ext;
+            string imageName = null;
+            if (upload != null)
+            {
+                indexExt = upload.FileName.IndexOf(".");
+                ext = upload.FileName.Substring(indexExt);
+                imageName = Guid.NewGuid() + ext;
+            }
+            return imageName;
+        }
+
+        public void UploadToAzure(string imageName, IFormFile upload)
+        {
+            // Azure needs your connection string like a db
+            string connectionString = Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING");
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                connectionString = _config.GetValue<string>("AzureConnectionString");
+            }
+            BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+            // Azure needs to know what folder you want to save in
+            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(_config.GetValue<string>("AzureImageFolder"));
+            // Then, you can get a blob writer thing from azure
+            containerClient.UploadBlob(imageName, upload.OpenReadStream());
+        }
+
+        public void DeleteFromAzure(string imageName)
+        {
+            // need connection to Azure just like a local db
+            string connectionString = Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING");
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                connectionString = _config.GetValue<string>("AzureConnectionString");
+            }
+            // connect to Azure
+            BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+            // go to a specific folder
+            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(_config.GetValue<string>("AzureImageFolder"));
+            // find a specific file
+            BlobClient blob = containerClient.GetBlobClient($"{imageName}");
+            // delete if exists
+            if (blob.Exists())
+            {
+                blob.Delete();
+            }
         }
     }
 }
